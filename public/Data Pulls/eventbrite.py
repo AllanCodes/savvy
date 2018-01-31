307b38462633bfc36406fc7ad359e98d07600fa5
Farshad Feyzi <feyzif@uci.edu>
2018-01-31 12:32:06 -0800

    Updated version of Eventbrite API pull

--- public/Data Pulls/eventbrite.py
+++ public/Data Pulls/eventbrite.py
@@ -18,6 +18,7 @@ class eventBrite():
         """
         ep = self.base_url + "categories/" + self.token
         r = requests.get(ep, verify=True)
+        categories = []
         if (r.status_code == 200):
             cats = dict({})
             l_ = r.json()["categories"]
@@ -25,26 +26,27 @@ class eventBrite():
             for category in l_:
                 count += 1
                 if (category["name"] != None):
+                    categories.append((category["name"], category["id"]))
                     d_ = {
                         "category": category["name"],
                         "id": category["id"]
                     }
                     cats[category["name"]] = d_
             cats["category_count"] = count
-            return cats
+            return cats, categories
 
-
-    def events(self, city="Irvine", within="30", category=""):
+    def events(self, city="Irvine", within="10", categoryID="", category=""):
         """
         Grab all events in area, within specified radius
         """
-        ep = self.base_url + "events/search/" + self.token + "&location.address=" + city + "&location.within=" + str(within) + "mi" + "&categories=" + str(category)
+        ep = self.base_url + "events/search/" + self.token + "&location.address=" + city + "&location.within=" + str(within) + "mi" + "&categories=" + str(categoryID)
         r = requests.get(ep, verify=True)
         if (r.status_code == 200):
             events = dict({})
+            events["id"] = categoryID
             pages = int(r.json()["pagination"]["page_count"])
             for x in range(pages):
-                ep = self.base_url + "events/search/" + self.token + "&location.address=" + city + "&page=" + str(x) + "&location.within=" + str(within) + "mi" + "&categories=" + str(category)
+                ep = self.base_url + "events/search/" + self.token + "&location.address=" + city + "&page=" + str(x) + "&location.within=" + str(within) + "mi" + "&categories=" + str(categoryID)
                 l_ = r.json()["events"]
                 count = 0
                 for event in l_:
@@ -55,7 +57,7 @@ class eventBrite():
                             "end_time": event["end"]["local"],
                             "url": event["url"],
                             "alive": event["status"],
-                            "description": event["description"]["text"]                 
+                            "description": event["description"]["text"]
                     }
                     title = event["name"]["text"].translate(str.maketrans('','','$#[]/.'))
                     events[title] = d_
@@ -72,10 +74,17 @@ class eventBrite():
 
 
 if __name__ == "__main__":
-   #pass
-    # p = eventBrite()
-    # p.categories()
-    # p.events()
+    print("Categorizing...")
+    p = eventBrite()
+    _,categories = p.categories()
 
-    # p.writeJson(p.categories(), "d.json")
-    # p.writeJson(p.events(), "events.json")
+    print("Collecting event data...")
+    events = dict({})
+    for cat, ids in categories:
+        events[cat] = p.events(categoryID = ids, category = cat)
+
+    print("Writing JSON...")
+    p.writeJson(p.categories(), "d.json")
+    p.writeJson(events, "events.json")
+
+    print("Complete")
