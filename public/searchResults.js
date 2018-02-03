1725453297a26c62552926ee326fcbc834de03e9
AllanCodes <Allaneivazian1@gmail.com>
2018-02-02 18:30:29 -0800

    Proper update

--- public/searchResults.js
+++ public/searchResults.js
@@ -1,11 +1,12 @@
 /**
  * 
  * searchResults.js
- * Grab input from search bar and provide results from Firebase
+ * General set up functions and search
  */
 
+    //global variable to make dynamic autocomplete work with changing categories
     let globalAutoComplete = null;
-
+      
     /**
      * Toggles all buttons in the category section to their opposite current value
      */
@@ -70,6 +71,34 @@
         $('#categoriesModal').modal('hide');    
     }
 
+   
+    function grabQueriedEvent() {
+            let eventRef = firebase.database().ref("/events");
+            let catRef = firebase.database().ref("/categories");
+            let categories = [];
+            let query = $('#search').val();
+            catRef.once("value", function(s) {
+                s.forEach(function(cat) {
+                    if (cat.val().category !== undefined)
+                        categories.push(cat.val().category);
+                });
+                eventRef.once("value", function(p) {
+                    categories.forEach(function(c, idx) {
+                        p.child(c).forEach(function(event, idx) {
+                            if (event.val().name !== undefined && (String(event.val().name.replace(/ /g, '')) === query.replace(/ /g, ''))) {
+                                $('#event_name').text(String(event.val().name));
+                                $('#event_url').text(String(event.val().url));
+                                $('#event_url').attr('href', String(event.val().url));
+                                $('#event_start').text(String(event.val().start_time));
+                                $('#event_description').text(String(event.val().description));
+                                $('#eventModal').modal('toggle');
+                            }
+                        });
+                    });
+                });
+            });
+        }   
+
     /**
      * create category check boxes in "show categories"
      */
@@ -92,7 +121,9 @@
             console.log(count + " categories added");
         });
     }
-    
+
+  
+
     function loadData() {
         let db = firebase.database();
         let eventsRef = db.ref("/events");
